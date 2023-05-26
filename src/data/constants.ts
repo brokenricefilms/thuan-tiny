009f74c566ec438e11332bdda45b43309cf273b5
Thuan Pham <thuanowa@gmail.com>
2023-05-26 17:15:34 +0700

    feat: use

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,27 +1,31 @@
 export const MENU_LINKS = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about",
-	},
-	{
-		title: "Blog",
-		path: "/posts",
-	},
-	{
-		title: "YTweets",
-		path: "https://www.youtube.com/@thuanowa/community",
-	},
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/posts",
+  },
+  {
+    title: "YTweets",
+    path: "https://www.youtube.com/@thuanowa/community",
+  },
+  {
+    title: "Use",
+    path: "https://use.thuanowa.com",
+  },
 ];
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/thuanowa",
-	discord: "https://discord.com/channels/@me/982102366255988776",
-	youtube: "https://youtube.com/@thuanowa",
-	figma: "https://figma.com/@thuanowa",
-	email: "thuanowa@gmail.com",
+  github: "https://github.com/thuanowa",
+  discord: "https://discord.com/channels/@me/982102366255988776",
+  youtube: "https://youtube.com/@thuanowa",
+  figma: "https://figma.com/@thuanowa",
+  email: "thuanowa@gmail.com",
 };
